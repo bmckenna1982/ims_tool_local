0dfe0f93f520c07c936bfb1a0930052fa8d00fc7
MitchGianoni <mitchgianoni@yahoo.com>
2019-12-23 22:38:02 -0500

    add landing page and some styles

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import AppContext from '../utils/appContext'
+import './styles/landingPage.scss'
 
 export default class LandingPage extends React.Component {
+  state = {
+    manufacturers: []
+  };
+
+  static contextType = AppContext;
+
   render() {
-    return (<div>
-      
+    const mfgs = [...new Set(this.context.models.map(model => model.manufacturer))];
+
+    return (<div className="landing-page">
+      <h1>Manufactuers</h1>
+      <ul className="mfg-list">
+        { mfgs.map(mfg => (
+            <li key={ mfg }>
+              <Link to={ {
+                pathname: `/api/mfg/${ mfg }`
+              } }>{ mfg }</Link>
+            </li>
+        )) }
+      </ul>
     </div>)
   }
 }
