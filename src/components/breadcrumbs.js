25899459cfc311671fece8775927215262a19303
bmckenna1982 <bmckenna1982@gmail.com>
2020-01-08 13:56:56 -0500

    updated styles for alignment of cart and list headings and icon text

--- src/components/breadcrumbs.js
+++ src/components/breadcrumbs.js
@@ -44,7 +44,7 @@ export default class Breadcrumbs extends React.Component {
                 crumb
             ))}
           </div>
-          <span className="icon_cart" onClick={this.handleClick}>
+          <span className="icon icon_cart" onClick={this.handleClick}>
             <FontAwesomeIcon icon={faShoppingCart}/>
             <p className="icon_text">Scroll to Cart</p>            
           </span>
--- src/components/cart.js
+++ src/components/cart.js
@@ -35,16 +35,17 @@ class Cart extends React.Component {
           <CartList shopCart={this.props.shopCart}/>
           <hr/>
           <Total shopCart={this.props.shopCart}/>
+          <p className="Cart_disclaimer">These prices were good when published. They can be used in discussions with the customer. However, ensure the customer knows that a final price must be obtained from IBM. Final prices could be lower or higher.</p>
         </div>
         : <div className='Cart_contents'>
-          <span>Your cart is empty</span>
+          <p>Your cart is empty</p>
         </div>
 
     return (
         <section className="Cart">
           <div className="cart-title">
             <h2>Your cart</h2>
-            <span className="icon_arrow" onClick={this.handleClick}>
+            <span className="icon icon_arrow" onClick={this.handleClick}>
             <FontAwesomeIcon icon={faArrowUp}/>
             <p className="icon_arrow_text">Return to Top</p>
           </span>
